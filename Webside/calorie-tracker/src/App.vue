4c66a0d9b38ee72262882a67620ed3ad4baa39d7
Lini1310 <linus.woerndle@gmail.com>
2023-12-06 10:14:59 +0100

    Full install
    Projekthandbuch added, Vue.js installed. Now we are going to code!

---
+++ Webside/calorie-tracker/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login-container">
+    <form @submit.prevent="onSubmit">
+      <input type="email" v-model="email" placeholder="E-Mail" />
+      <input type="password" v-model="password" placeholder="Passwort" />
+      <button type="submit">Anmelden</button>
+      <button type="button">Registrieren</button>
+    </form>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
